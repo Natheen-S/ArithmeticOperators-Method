7aff3b3e59b1607c32a58dd3f42b0e55290cc332
Natheen-S <96934524+Natheen-S@users.noreply.github.com>
2023-01-02 10:52:39 +0530

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArithmeticOperators_Method
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_plus_Click(object sender, RoutedEventArgs e)
+        {
+            int one = Convert.ToInt32(txtbox_one.Text);
+            int two = Convert.ToInt32(txtbox_two.Text);
+            int result = artithmeticoper(one,two);
+            txtbox_three.Text= result.ToString();
+        }
+
+        private int artithmeticoper(int a, int b) 
+        {
+           
+            int result ;
+            if ( btn_plus.IsPressed ==true )
+            {
+                result = a + b;
+                return result;
+            }
+            if (btn_minus.IsPressed ==true )
+            {
+                result = a - b;
+                return result;
+            }
+            if ( btn_multiply.IsPressed ==true)
+            {
+                result = a * b;
+                return result;
+            }
+            
+            
+            
+        }
+
+    }
+}
